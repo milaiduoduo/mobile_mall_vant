56262134ad2c3d8780725dbc62b1710387a87d18
advancingKoala <milaiduoduo4567@163.com>
2018-09-05 18:42:13 +0800

    [分类页]，新增右边的商品展示布局，采用flex布局方式，目前的数据是手造数据。

--- src/components/categoryTree/categoryTree.vue
+++ src/components/categoryTree/categoryTree.vue
@@ -8,12 +8,77 @@
             </ul>
         </div>
         <div class="treeContent">
-          
+          <div class="all">全部<van-icon  name="arrow"></van-icon></div>
+          <div class="itemWrap">
+          <div class="item" v-for="(item1,index) in categoryItems" :key=index>
+            <div>
+              <img  width="80px" height="80px" src="http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg" alt="凉鞋" >
+            </div>
+            <div>{{item1.title}}</div>
+          </div>
+          </div>
         </div>
     </div>
 </template>
 
 <script type="text/ecmascript-6">
+let categoryItemData = [
+  {
+    imgUrl:
+      "http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "http://img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  },
+  {
+    imgUrl:
+      "img30.360buyimg.com/popWaterMark/jfs/t8365/71/665713975/145325/69f3da07/59afa756Nf19bc011.jpg",
+    title: "cross beya"
+  }
+];
 export default {
   props: {
     categorys: {
@@ -25,7 +90,8 @@ export default {
   },
   data() {
     return {
-      categoryActive: 0
+      categoryActive: 0,
+      categoryItems: categoryItemData
     };
   },
   methods: {
@@ -40,25 +106,45 @@ export default {
 @import "../../assets/scss/var";
 .wrap {
   height: 100%;
-  .itemTree {
-    width: 100px;
-    height: 100%;
-    // background: cadetblue;
-    li {
-      height: 40px;
-      line-height: 40px;
-      text-align: center;
-      border-left: 2px solid $bg-color;
-    }
-    li.active_category {
-      background: $white;
-      border-left: 2px solid $main-color;
-    }
+}
+.itemTree {
+  width: 100px;
+  height: 100%;
+  // background: cadetblue;
+  li {
+    height: 40px;
+    line-height: 40px;
+    text-align: center;
+    border-left: 2px solid $bg-color;
   }
-  .treeContent {
-    height: 100%;
-    margin-left: 100px;
-    background: pink;
+  li.active_category {
+    background: $white;
+    border-left: 2px solid $main-color;
+  }
+}
+.treeContent {
+  height: 100%;
+  margin-left: 100px;
+  text-align: center;
+  background: $white;
+  .all {
+    height: 40px;
+    line-height: 40px;
+    text-align: right;
+    padding: 0 10px;
+    background: $main-color-light;
+    color: $white;
+  }
+  .itemWrap {
+    display: flex;
+    flex-wrap: wrap;
+    padding: 10px 2px;
+    .item {
+      box-sizing: border-box;
+      flex: 0 1 90px;
+      margin-bottom: 10px;
+      text-align: center;
+    }
   }
 }
 </style>
