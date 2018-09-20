36b4e140f1e839f56f8a04b241a4c19cc5118baf
advancingKoala <milaiduoduo4567@163.com>
2018-09-20 11:05:42 +0800

    vue2.4新增特性，$attrs和$listensers的使用

---
+++ src/views/test/testAttrsListeners/child1.vue
@@ -0,0 +1,12 @@
+<template>
+    <h3>{{a1}}</h3>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  props: ["a1"]
+};
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+</style>
---
+++ src/views/test/testAttrsListeners/parent.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>我是parent</h1>
+<child1 :a1="a1" :b1="b1"></child1>
+    </div>
+    
+</template>
+
+<script type="text/ecmascript-6">
+import child1 from "./child1.vue";
+export default {
+  components: { child1 },
+  data() {
+    return {
+      a1: "hellow a1!",
+      b1: "hellow b1!"
+    };
+  }
+};
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+</style>
