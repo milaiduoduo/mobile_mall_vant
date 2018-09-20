6b9e92178d756226cd0cd128b4523fa9f45e27d9
advancingKoala <milaiduoduo4567@163.com>
2018-09-20 11:24:23 +0800

    打印$attrs，证明child1.vue中可以通过$attrs获取未被child1.vue使用的props.

--- src/views/test/testAttrsListeners/child1.vue
+++ src/views/test/testAttrsListeners/child1.vue
@@ -1,6 +1,8 @@
 <template>
    <div class="child1Wrap">
-    <h3>{{a1}}</h3>
+     <h2>我是child1.vue!!!</h2>
+    <h2>1.被child1.vue使用的props a1: {{a1}}</h2>
+    <h2>2.剩余未被使用的attrs有: {{$attrs}}</h2>
    </div>
 
 </template>
@@ -8,13 +10,19 @@
 <script type="text/ecmascript-6">
 export default {
   props: ["a1"],
-  inheritAttrs: false //设置inheritAttrs属性为false，父组件传递下来的未被child1.vue使用的props将不会在应用到子组件的根元素上。
+  inheritAttrs: false, //设置inheritAttrs属性为false，父组件传递下来的未被child1.vue使用的props将不会在应用到子组件的根元素上。
   //应用前：
   //<div data-v-42136c0c="" data-v-356298be="" class="child1Wrap" b1="hellow b1!"><h3 data-v-42136c0c="">hellow a1!</h3></div>
   //应用后：
   //<div data-v-42136c0c="" data-v-356298be="" class="child1Wrap"><h3 data-v-42136c0c="">hellow a1!</h3></div>
+  created() {
+    console.log(this.$attrs);
+  }
 };
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
+.child1Wrap {
+  background: cadetblue;
+}
 </style>
--- src/views/test/testAttrsListeners/parent.vue
+++ src/views/test/testAttrsListeners/parent.vue
@@ -2,7 +2,8 @@
 <div>
     <div class="parentWrap">
         <h1>我是parent</h1>
-<child1 :a1="a1" :b1="b1"></child1>
+        <h1>我设置了4个attr,a1,b1,c1,d1</h1>
+<child1 :a1="a1" :b1="b1" :c1="c1" :d1="d1"></child1>
     </div>
 </div>
     
@@ -16,7 +17,9 @@ export default {
   data() {
     return {
       a1: "hellow a1!",
-      b1: "hellow b1!"
+      b1: "hellow b1!",
+      c1: "hellow c1!",
+      d1: "hellow d1!"
     };
   }
 };
